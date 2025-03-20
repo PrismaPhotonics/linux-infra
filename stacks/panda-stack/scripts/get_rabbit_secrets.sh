0c94f7b3c0260c225c3bca3b224b9291c3f33c57
Benny Koren <benny.koren@prismaphotonics.com>
2025-03-20 10:44:39 +0000

    add rabbit secrets

--- stacks/panda-stack/scripts/get_rabbit_secrets.sh
+++ stacks/panda-stack/scripts/get_rabbit_secrets.sh
@@ -1,4 +1,5 @@
 # Debug: Print environment variables
+echo "*******************ENVIRONMENT VARIABLES*******************"
 env | grep -E 'AWS|S3'
 
 # Define secrets path
@@ -7,16 +8,19 @@ SECRET_NAME="rabbitmq-tls-secret"
 TEMP_DIR="/tmp/rabbitmq-secrets"
 mkdir -p $TEMP_DIR
 
+echo "*******************Start Downloading*******************"
 
 # Test AWS credentials
 sudo aws sts get-caller-identity || echo "ERROR: AWS credentials not found!"
 
 # Download secrets from S3
-sudo aws s3 cp s3://pz-devops/deploy/deployment/ui/panda-certifcates/ca.pem ca.pem
-sudo aws s3 cp s3://pz-devops/deploy/deployment/ui/panda-certifcates/private.pem private.pem
-sudo aws s3 cp s3://pz-devops/deploy/deployment/ui/panda-certifcates/public.pem public.pem
+sudo aws s3 cp s3://pz-devops/deploy/deployment/ui/panda-certifcates/ca.pem $TEMP_DIR/ca.pem
+sudo aws s3 cp s3://pz-devops/deploy/deployment/ui/panda-certifcates/private.pem $TEMP_DIR/private.pem
+sudo aws s3 cp s3://pz-devops/deploy/deployment/ui/panda-certifcates/public.pem $TEMP_DIR/public.pem
 
+echo "*******************Finish Downloading*******************"
 ls -lah $TEMP_DIR
+echo "*******************Finish Listing Files*******************"
 
 # Create Kubernetes secret
 kubectl delete secret $SECRET_NAME -n $SECRET_NAMESPACE --ignore-not-found
