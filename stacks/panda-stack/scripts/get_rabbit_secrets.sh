7304d28e22a2f7e086a0ff003fb6353f6677f07a
Benny Koren <benny.koren@prismaphotonics.com>
2025-03-20 09:43:47 +0000

    add rabbit secrets

--- stacks/panda-stack/scripts/get_rabbit_secrets.sh
+++ stacks/panda-stack/scripts/get_rabbit_secrets.sh
@@ -1,16 +1,26 @@
+# Debug: Print environment variables
+echo "*******************ENVIRONMENT VARIABLES*******************"
+env | grep -E 'AWS|S3'
+
 # Define secrets path
 SECRET_NAMESPACE="rabbitmq-panda"
 SECRET_NAME="rabbitmq-tls-secret"
 TEMP_DIR="/tmp/rabbitmq-secrets"
 mkdir -p $TEMP_DIR
-echo "*******************start to downlaod*******************"
+
+echo "*******************Start Downloading*******************"
+
+# Test AWS credentials
+aws sts get-caller-identity || echo "ERROR: AWS credentials not found!"
+
 # Download secrets from S3
-aws s3 cp s3://pz-devops/deploy/deployment/ui/panda-certifcates/ca.pem /etc/panda-certificates/ca.pem
-aws s3 cp s3://pz-devops/deploy/deployment/ui/panda-certifcates/private.pem /etc/panda-certificates/private.pem
-aws s3 cp s3://pz-devops/deploy/deployment/ui/panda-certifcates/public.pem /etc/panda-certificates/public.pem
-echo "*******************finish to downlaod*******************"
-ls $TEMP_DIR
-echo "*******************finish to ls*******************"
+s3cmd --debug get s3://pz-devops/deploy/deployment/ui/panda-certifcates/ca.pem $TEMP_DIR/ca.pem
+s3cmd --debug get s3://pz-devops/deploy/deployment/ui/panda-certifcates/private.pem $TEMP_DIR/private.pem
+s3cmd --debug get s3://pz-devops/deploy/deployment/ui/panda-certifcates/public.pem $TEMP_DIR/public.pem
+
+echo "*******************Finish Downloading*******************"
+ls -lah $TEMP_DIR
+echo "*******************Finish Listing Files*******************"
 
 # Create Kubernetes secret
 kubectl delete secret $SECRET_NAME -n $SECRET_NAMESPACE --ignore-not-found
@@ -18,3 +28,6 @@ kubectl create secret generic $SECRET_NAME -n $SECRET_NAMESPACE \
   --from-file=ca.pem=$TEMP_DIR/ca.pem \
   --from-file=private.pem=$TEMP_DIR/private.pem \
   --from-file=public.pem=$TEMP_DIR/public.pem
+
+# Cleanup
+rm -rf $TEMP_DIR
