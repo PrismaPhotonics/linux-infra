d73a789196f1f98549580ad308530f885bcd0af8
Benny Koren <benny.koren@prismaphotonics.com>
2025-03-20 10:57:06 +0000

    add rabbit secrets

--- stacks/panda-stack/scripts/get_rabbit_secrets.sh
+++ stacks/panda-stack/scripts/get_rabbit_secrets.sh
@@ -14,12 +14,12 @@ echo "*******************Start Downloading*******************"
 sudo aws sts get-caller-identity || echo "ERROR: AWS credentials not found!"
 
 # Download secrets from S3
-sudo aws s3 cp s3://pz-devops/deploy/deployment/ui/panda-certifcates/ca.pem $TEMP_DIR/ca.crt
-sudo aws s3 cp s3://pz-devops/deploy/deployment/ui/panda-certifcates/private.pem $TEMP_DIR/private.key
-sudo aws s3 cp s3://pz-devops/deploy/deployment/ui/panda-certifcates/public.pem $TEMP_DIR/public.crt
+sudo aws s3 cp s3://pz-devops/deploy/deployment/ui/panda-certifcates/ca.pem $TEMP_DIR/ca.pem
+sudo aws s3 cp s3://pz-devops/deploy/deployment/ui/panda-certifcates/private.pem $TEMP_DIR/private.pem
+sudo aws s3 cp s3://pz-devops/deploy/deployment/ui/panda-certifcates/public.pem $TEMP_DIR/public.pem
 
 echo "*******************Finish Downloading*******************"
-sudo ls -la $TEMP_DIR
+ls -la $TEMP_DIR
 echo "*******************Finish Listing Files*******************"
 
 # Create Kubernetes secret
